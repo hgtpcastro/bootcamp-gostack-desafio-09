187deaf0387a233dff9149b159d5d5ff10f4a419
Hugo Castro Costa <hugo@miaritecnologia.com.br>
2020-03-19 09:19:40 -0300

    using @rocketseat/unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/fastfeet-logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
-        <input type="email" placeholder="exemplo@email.com" />
-        <input type="password" placeholder="*************" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <Input name="password" type="password" placeholder="*************" />
         <button type="submit">Entrar no sistema</button>
-      </form>
+      </Form>
     </>
   );
 }
